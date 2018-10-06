02edc64936f25fb16d6c701726f8363207d350be
ShatMs <shatms100@gmail.com>
2018-10-06 12:32:53 +0300

    03_styles

---
+++ 03_Styles/js/script.js
@@ -0,0 +1,5 @@
+const Colored = document.querySelector('.colored');
+console.log(Colored);
+console.log(Colored.style);
+Colored.style.backgroundColor = 'blue';
+Colored.style['background-color'] = 'green';
